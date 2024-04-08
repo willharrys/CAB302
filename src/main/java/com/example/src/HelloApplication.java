bb56b05b74f89ce875b4a005bad078df8e6730cc
Cooper <cooperrose128@gmail.com>
2024-04-08 14:43:11 +1000

    Started the project by setting background color with a slider and text area

--- src/main/java/com/example/src/HelloApplication.java
+++ src/main/java/com/example/src/HelloApplication.java
@@ -3,18 +3,29 @@ package com.example.src;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    //Defining the window title and size
+    public static final String TITLE = "Journal App";
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 800;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(),WIDTH, HEIGHT);
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/src/HelloController.java
+++ src/main/java/com/example/src/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.src;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+import java.text.BreakIterator;
+import java.text.NumberFormat;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextArea Clear;
+    @FXML
+    private Button Submit;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+       Submit.setText("Thanks for submitting!");
+       Clear.clear();
+
     }
 }
