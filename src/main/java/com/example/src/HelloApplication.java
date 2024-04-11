fba99a52467fb8e33af05f521e8c7bbd382de32d
Alexander Cuciureanu <alexander.cuciureanu@connect.qut.edu.au>
2024-04-11 13:41:05 +1000

    Changed the project image to Shrek

--- src/main/java/com/example/src/HelloApplication.java
+++ src/main/java/com/example/src/HelloApplication.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -18,9 +20,11 @@ public class HelloApplication extends Application {
     public static final String TITLE = "Journal App";
     public static final int WIDTH = 800;
     public static final int HEIGHT = 650;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        stage.getIcons().add(new Image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrNCnyiMs2nC5fTCEX6y0IWV6yoznFqLl-1qYMEkbodQ&s"));
         Scene scene = new Scene(fxmlLoader.load(),WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
