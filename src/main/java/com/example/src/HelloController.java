26267049db6ea839c849888a852707c83436c236
Cooper <cooperrose128@gmail.com>
2024-04-11 14:45:34 +1000

    Made a next page button that works

--- src/main/java/com/example/src/HelloController.java
+++ src/main/java/com/example/src/HelloController.java
@@ -2,11 +2,15 @@ package com.example.src;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.text.BreakIterator;
 import java.text.NumberFormat;
 
@@ -16,7 +20,7 @@ public class HelloController {
     @FXML
     private Button Submit;
     @FXML
-    private Button Resubmit;
+    private Button NextPage;
 
 
     @FXML
@@ -30,5 +34,15 @@ public class HelloController {
     @FXML
     protected void ResubmitButtonClick(){
         Submit.setText("Submit!");
+    }
+
+    @FXML
+    protected void NextPageClick()
+        throws IOException {
+            Stage stage = (Stage) NextPage.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+            stage.setScene(scene);
+
     }
 }
