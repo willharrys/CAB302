c69d99b998ffe7c6b0e2f8e9d6e818574ab69458
Alexander Cuciureanu <alexander.cuciureanu@connect.qut.edu.au>
2024-04-16 23:52:11 +1000

    Resized the login and register pages to the correct dimensions

--- src/main/java/com/example/src/StartController.java
+++ src/main/java/com/example/src/StartController.java
@@ -7,7 +7,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/*
+This starts the application
+ */
 
 public class StartController {
 
@@ -16,13 +18,15 @@ public class StartController {
 
     @FXML
     private Button signUpButton;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 650;
+
 
     @FXML
     private void handleLogin(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         Stage stage = (Stage) loginButton.getScene().getWindow();
-        Scene scene = new Scene(root);
-
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.show();
     }
@@ -31,7 +35,7 @@ public class StartController {
     private void handleSignUp(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sign-up.fxml"));
         Stage stage = (Stage) signUpButton.getScene().getWindow();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.show();
     }
