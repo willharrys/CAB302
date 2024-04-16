e3802f79ef8a6ac9c21dbe1e626d6ca9d106c695
Cooper <cooperrose128@gmail.com>
2024-04-16 18:19:22 +1000

    added homepage section with menu, as well as emotion page and support section

---
+++ src/main/java/com/example/src/MenuController.java
@@ -0,0 +1,46 @@
+package com.example.src;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML private Button homeMenu;
+    @FXML private Button emotionMenu;
+    @FXML private Button supportMenu;
+    @FXML
+    protected void homeMenuClick()
+            throws IOException {
+        Stage stage = (Stage) homeMenu.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+
+    }
+    @FXML
+    protected void emotionMenuClick()
+            throws IOException {
+        Stage stage = (Stage) emotionMenu.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("emotions.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+
+    }
+    @FXML
+    protected void supportMenuClick()
+            throws IOException {
+        Stage stage = (Stage) supportMenu.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("support.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+
+    }
+
+}
---
+++ src/main/java/com/example/src/SupportController.java
@@ -0,0 +1,4 @@
+package com.example.src;
+
+public class SupportController {
+}
