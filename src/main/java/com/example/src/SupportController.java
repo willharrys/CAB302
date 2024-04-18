390fd2a630662a62abfb4e7e41f51ba55efa46dc
Cooper <cooperrose128@gmail.com>
2024-04-18 13:26:51 +1000

    ADDED MORE LINKSSS!!!

--- src/main/java/com/example/src/SupportController.java
+++ src/main/java/com/example/src/SupportController.java
@@ -15,4 +15,12 @@ public class SupportController extends MenuController {
     public void openLink(ActionEvent event) throws URISyntaxException, IOException {
         java.awt.Desktop.getDesktop().browse(new URI("http://www.google.com"));
     }
+
+    public void openLink2(ActionEvent event) throws URISyntaxException, IOException {
+        java.awt.Desktop.getDesktop().browse(new URI("https://www.beyondblue.org.au/"));
+    }
+    public void openLink3(ActionEvent event) throws URISyntaxException, IOException{
+        java.awt.Desktop.getDesktop().browse(new URI("https://kidshelpline.com.au/"));
+    }
+
 }
