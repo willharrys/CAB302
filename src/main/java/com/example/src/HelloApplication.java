309f6d0d2592cd34504afbdbac5d5217a2a0e198
Lachlan Ross <n11550287@qut.edu.au>
2024-04-27 19:31:12 +1000

    Updated UI on all pages to fit with the colour palette.

--- src/main/java/com/example/src/HelloApplication.java
+++ src/main/java/com/example/src/HelloApplication.java
@@ -29,7 +29,6 @@ public class HelloApplication extends Application {
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
